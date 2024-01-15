0abf04df7426578742341fc0cbe4918fa61ae73d
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2024-01-15 20:15:12 +0000

    Random input works/styled pages

--- app.rb
+++ app.rb
@@ -8,9 +8,11 @@ end
 
 get '/random_photo' do
   # Logic to generate and display a random photo
+  user_input = params[:user_input]
   api_key = 'ART_API_KEY'
   url = URI.parse("https://source.unsplash.com/random?api_key=#{api_key}")
-  @random_photo_url = "https://source.unsplash.com/random" 
+  @random_photo_url = "https://source.unsplash.com/random?/#{user_input}" 
+  
   
   erb :random_photo
 end
